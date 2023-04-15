1e0f2d34b4ac7e8258da0e03a10e1993ac28ff66
Genius5tar <101893819+Genius5tar@users.noreply.github.com>
2023-04-15 13:58:48 -0500

    Agregue registrar usuarios nuevos

--- view/login.php
+++ view/login.php
@@ -28,9 +28,9 @@
 
     <!-- Login Form -->
     <form method="POST">
-      <input type="text" id="user" class="fadeIn second" name="user" placeholder="User">
-      <input type="password" id="pass" class="fadeIn third" name="pass" placeholder="Password">
+      <input type="text" id="user" class="fadeIn second" name="user" placeholder="User">   
       <input type="submit" class="fadeIn fourth" name="btn_login" value="login">
+      <input type="submit" class="fadeIn fourth" name="btn_regist" value="Registro">
     </form>
 
     <!-- Remind Passowrd -->
